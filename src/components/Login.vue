bb2a654aa4a2791744b87d98de6aa5c3db24ee5a
Kyle <kyleplatt2014@gmail.com>
2018-02-11 17:03:55 -0500

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="Email"><br>
+    <input type="password" placeholder="Password"><br>
+    <button v-on:click="login">Connection</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data: function() {
+      return {}
+    },
+    methods: {
+      login: function() {
+        this.$router.replace('home')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.login {
+  margin-top: 40px;
+}
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+button {
+  margin-top: 20px;
+  width: 10%;
+  cursor: pointer;
+}
+</style>
